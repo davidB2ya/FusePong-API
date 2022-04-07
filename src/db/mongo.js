6f5663ce28c73ec535e32569404a7b24c1d3c997
davidB2ya <dbedoya0408@gmail.com>
2022-04-07 17:18:41 -0500

    Fixed Deploy

--- src/db/mongo.js
+++ src/db/mongo.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
 
-const connectionString = process.env.MONGO_DB_URI;
+const { MONGO_DB_URI } = process.env
+
+const connectionString = MONGO_DB_URI;
 
 if (!connectionString) {
     console.error(
         'Remember that you must have a .env file with the environment variables defined and the MONGO_DB_URI'
-    );
-};
+    )
+}
 
 // Conection a mongoDB
 mongoose
@@ -14,18 +16,16 @@ mongoose
         // it allow users to fall back to the old parser if they find a bug in the new parser.
         useNewUrlParser: true,
         // it allows to use the new connection management engine of the MongoDB driver
-        useUnifiedTopology: true,
+        useUnifiedTopology: true
     })
     .then(() => {
-        console.log('Database connected');
+        console.log('Database connected')
     })
     .catch(err => {
-        console.error("Error connected BBDD");
-        console.log("MONGO_DB_URI: " + connectionString)
-        console.error(err);
-    });
+        console.log(err)
+    })
 
 process.on('uncaughtException', error => {
-    console.error(error);
-    mongoose.disconnect();
-});
+    console.error(error)
+    mongoose.disconnect()
+})
